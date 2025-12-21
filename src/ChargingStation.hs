7b1a5fa2e6ba325bf2aac218e99c4f4588902064
Philippe Crama <pcfeb0009@gmx.com>
2025-12-21 21:43:54 +0100

    Clean up code, using built-in (w, a) as Writer w a

--- src/ChargingStation.hs
+++ src/ChargingStation.hs
@@ -1,36 +1,84 @@
 module ChargingStation
   ( stepSimulation,
     SessionTarget (..),
-    Timestamp,
+    Timestamp (..),
     Duration,
     MeterValues (..),
     MeterValuesStateMachine (..),
+    OutputEvent (..),
     Phases (..),
     SessionState (..),
     SessionConfiguration (..),
     Session (..),
     SimState (..),
+    seconds,
+    minutes,
+    durationUntil,
+    after,
     chargeEfficientlyUntil80Percent,
-    instantaneousCurrentMaxUntil80Percent,
+    getInstantaneousCurrentMaxUntil80Percent,
   )
 where
 
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.Writer (MonadWriter, tell)
+import Data.Bifunctor (first)
+import Data.Monoid ((<>))
 import Data.Text (Text)
 import Data.Word (Word64, Word8)
 
 -- | [ms] since some imaginary epoch
-type Timestamp = Word64
+newtype Timestamp = Timestamp Word64
+  deriving (Show, Eq, Ord)
 
-type Duration =
-  -- | [ms]
-  Word64
+newtype Duration
+  = Duration
+      -- | [ms]
+      Word64
+  deriving (Show, Eq, Ord)
+
+instance Semigroup Duration where
+  (Duration lf) <> (Duration rg) = Duration $ lf + rg
+
+instance Monoid Duration where
+  mempty = Duration 0
+
+after :: Duration -> Timestamp -> Timestamp
+(Duration dur) `after` (Timestamp ts) = Timestamp $ ts + dur
+
+before :: Duration -> Timestamp -> Timestamp
+(Duration dur) `before` (Timestamp ts)
+  | ts >= dur = Timestamp $ ts - dur
+  | otherwise = error "Timestamp underflow"
+
+durationUntil :: Timestamp -> Timestamp -> Duration
+durationUntil (Timestamp from_) (Timestamp to_)
+  | from_ <= to_ = Duration $ to_ - from_
+  | otherwise = error "Time must always go forward"
+
+clampingDurationUntil :: Timestamp -> Timestamp -> Duration
+clampingDurationUntil (Timestamp from_) (Timestamp to_)
+  | from_ <= to_ = Duration $ to_ - from_
+  | otherwise = Duration 0
+
+secondsFrom :: Duration -> Double
+secondsFrom (Duration dur) = fromIntegral dur / 1000.0
+
+minutes :: Word64 -> Duration
+minutes = Duration . (* 60000)
+
+seconds :: Word64 -> Duration
+seconds = Duration . (* 1000)
+
+milliseconds :: Word64 -> Duration
+milliseconds = Duration
 
 data SessionTarget
   = -- | driver wants to leave at a fixed time
     LeaveAtTick !Timestamp
   | -- | driver wants to leave at the earliest after the given time but only if their charge level is sufficient
     LeaveAfterBoth !Timestamp !Double
-  | -- | driver will stay until the given time or as soon as their battery has reached that charge level
+  | -- | driver will stay until the given time or leave as soon as their battery has reached that charge level
     LeaveAfterEither !Timestamp !Double
   | -- | drive will stay until their battery has reached that charge level
     LeaveAtLevel !Double
@@ -43,11 +91,11 @@ data Phases
     S
   | -- | Mono on 3rd phase
     T
-  | -- | Two phases
+  | -- | Two phases (1+2)
     RS
-  | -- | Two phases
+  | -- | Two phases (2+3)
     ST
-  | -- | Two phases
+  | -- | Two phases (1+3)
     RT
   | -- | Three phases
     RST
@@ -66,7 +114,6 @@ data SessionState
         transactionId :: !Word64,
         meterValuesStateMachine :: !MeterValuesStateMachine
       }
-  | SuspendedEV
   | Finishing
   deriving (Show, Eq)
 
@@ -92,8 +139,9 @@ data SessionConfiguration = SessionConfiguration
   { -- | how many [Wh] "fit" in the car's battery
     batteryCapacity :: !Double,
     sessionTarget :: !SessionTarget,
-    charge :: Timestamp -> SessionState -> Duration -> SessionState,
-    instantaneousCurrent :: Timestamp -> SessionState -> (Double, Double, Double),
+    -- | update session state to account for passing of time
+    charge :: SessionState -> Duration -> Timestamp -> (OutputEvent [] MeterValues, SessionState),
+    getInstantaneousCurrent :: Timestamp -> SessionState -> (Double, Double, Double),
     phases :: !Phases,
     stationId :: !Text,
     connectorId :: !Word8,
@@ -102,43 +150,92 @@ data SessionConfiguration = SessionConfiguration
 
 data Session = Session !SessionConfiguration !SessionState
 
-data SimState = SimState
-  { tick :: !Timestamp,
-    session :: !Session
+data SimState = SimState {tick :: !Timestamp, session :: !Session}
+
+data InputEvent a = InputEvent
+  { ieTick :: !Timestamp,
+    ieTrigger :: !(Maybe a)
   }
+  deriving (Show, Eq)
+
+data OutputEvent w b
+  = NoEvent
+  | OutputEvent
+      { -- | Request for next step no later than after this duration
+        oeNext :: !Duration,
+        -- | Produce those output events `b` right now and next step no later than after this duration
+        oeEvent :: !(w b)
+      }
+  deriving (Show, Eq)
+
+instance (Semigroup (w b)) => Semigroup (OutputEvent w b) where
+  lf <> NoEvent = lf
+  NoEvent <> rg = rg
+  lf@(OutputEvent {oeNext = lfNxt, oeEvent = lfEvts}) <> rg@(OutputEvent {oeNext = rgNxt, oeEvent = rgEvts}) =
+    OutputEvent
+      { oeNext = min lfNxt rgNxt,
+        oeEvent = lfEvts <> rgEvts
+      }
+
+instance (Semigroup (w b)) => Monoid (OutputEvent w b) where
+  mempty = NoEvent
+
+tellNextSimulationStep :: (Monoid (w b), MonadWriter (OutputEvent w b) m) => Duration -> m ()
+tellNextSimulationStep dur = tell $ OutputEvent {oeNext = dur, oeEvent = mempty}
+
+tellNextSimulationStepAndEvent :: (MonadWriter (OutputEvent [] b) m) => Duration -> b -> m b
+tellNextSimulationStepAndEvent dur evt = do
+  tell $ OutputEvent {oeNext = dur, oeEvent = [evt]}
+  return evt
 
 countPhases :: (Double, Double, Double) -> Int
 countPhases (x, y, z) = (if x > 0.0 then 1 else 0) + (if y > 0.0 then 1 else 0) + (if z > 0.0 then 1 else 0)
 
 chargeEfficientlyUntil80Percent ::
+  -- | charging session configuration
   SessionConfiguration ->
-  Timestamp ->
+  -- | current charging session state
   SessionState ->
+  -- | duration of the time step to advance the session state
   Duration ->
-  SessionState
-chargeEfficientlyUntil80Percent _ _ Available _ = Available
-chargeEfficientlyUntil80Percent _ _ Preparing _ = Preparing
+  -- | timestamp of the new session state (i.e. timestamp of last session state + time step)
+  Timestamp ->
+  -- | new session state
+  (OutputEvent [] MeterValues, SessionState)
+chargeEfficientlyUntil80Percent _ Available _ _ = return Available
+chargeEfficientlyUntil80Percent _ Preparing _ _ = return Preparing
 chargeEfficientlyUntil80Percent
-  (SessionConfiguration {instantaneousCurrent, sessionTarget, batteryCapacity})
-  t0
+  (SessionConfiguration {getInstantaneousCurrent, sessionTarget, batteryCapacity})
   sessionState@(Charging {energyDelivered, batteryLevel, currentOffered})
-  dt =
+  dt
+  nextTime = do
+    let
     case sessionTarget of
-      LeaveAtTick ts | ts <= t0 + dt -> Available
-      LeaveAfterBoth ts targetLevel | (ts <= t0 + dt) && (targetLevel <= newLevel) -> Available
-      LeaveAfterEither ts targetLevel | (ts <= t0 + dt) || (targetLevel <= newLevel) -> Available
-      LeaveAtLevel targetLevel | targetLevel <= newLevel -> Available
-      _ -> sessionState {energyDelivered = newEnergyDelivered, batteryLevel = newLevel}
+      LeaveAtTick ts ->
+        if ts <= nextTime then return Available else accumulateEnergy $ nextTime `durationUntil` ts
+      LeaveAfterBoth ts targetLevel ->
+        if (ts <= nextTime) && (targetLevel <= newLevel) then return Available else accumulateEnergy $ nextTime `durationUntil` ts
+      LeaveAfterEither ts targetLevel ->
+        if (ts <= nextTime) || (targetLevel <= newLevel)
+          then return Available
+          else accumulateEnergy $ if ts <= nextTime then defaultNextSimulationStep else nextTime `durationUntil` ts
+      LeaveAtLevel targetLevel ->
+        if targetLevel <= newLevel then return Available else accumulateEnergy defaultNextSimulationStep
     where
+      instantaneousCurrent = getInstantaneousCurrent (dt `before` nextTime) sessionState
+      defaultNextSimulationStep = minutes 5
       voltage = 230
-      sumOfCurrents = let (x, y, z) = instantaneousCurrent t0 sessionState in x + y + z
-      deltaEnergyDelivered = (voltage * sumOfCurrents * fromIntegral dt) / 3600.0 / 1000.0
+      sumOfCurrents = let (x, y, z) = instantaneousCurrent in x + y + z
+      deltaEnergyDelivered = (voltage * sumOfCurrents * secondsFrom dt) / 3600.0 -- 3600 to convert from [J] to [Wh]
       batteryPercentage = batteryLevel / batteryCapacity
       chargingEfficiency = if batteryPercentage < 0.8 then 0.85 else 0.75
       newLevel = batteryLevel + deltaEnergyDelivered * chargingEfficiency
       newEnergyDelivered = energyDelivered + deltaEnergyDelivered
+      accumulateEnergy nextStep = do
+        tellNextSimulationStep nextStep
+        return sessionState {energyDelivered = newEnergyDelivered, batteryLevel = newLevel}
 
-instantaneousCurrentMaxUntil80Percent ::
+getInstantaneousCurrentMaxUntil80Percent ::
   Double ->
   Double ->
   SessionConfiguration ->
@@ -146,9 +243,10 @@ instantaneousCurrentMaxUntil80Percent ::
   Timestamp ->
   SessionState ->
   (Double, Double, Double)
-instantaneousCurrentMaxUntil80Percent _ _ _ _ Available = (0.0, 0.0, 0.0)
-instantaneousCurrentMaxUntil80Percent _ _ _ _ Preparing = (0.0, 0.0, 0.0)
-instantaneousCurrentMaxUntil80Percent
+getInstantaneousCurrentMaxUntil80Percent _ _ _ _ Available = (0.0, 0.0, 0.0)
+getInstantaneousCurrentMaxUntil80Percent _ _ _ _ Preparing = (0.0, 0.0, 0.0)
+getInstantaneousCurrentMaxUntil80Percent _ _ _ _ Finishing = (0.0, 0.0, 0.0)
+getInstantaneousCurrentMaxUntil80Percent
   maxCurrent
   offset
   (SessionConfiguration {batteryCapacity, phases})
@@ -169,75 +267,78 @@ instantaneousCurrentMaxUntil80Percent
       batteryCurrent = if batteryPercentage < knee then maxCurrent else minCurrent + (maxCurrent - minCurrent) * (1.0 - batteryPercentage) / (1 - knee)
       cappedCurrent = if currentOffered == 0 then 0 else max 0.1 $ min batteryCurrent $ currentOffered - offset
 
-stepSessionState ::
-  -- | tick of the SimState, associated with current SessionState value
-  Timestamp ->
-  -- | configuration of the session
-  SessionConfiguration ->
-  -- | session's state at t0
-  SessionState ->
-  -- | timestamp for which to compute new session state
-  Timestamp ->
-  SessionState
-stepSessionState t0 sessionConfiguration@(SessionConfiguration {instantaneousCurrent, meterValuesPeriodicity, stationId, connectorId}) sessionState ts
-  | ts > t0 = updateMeterValuesStateMachine $ charge sessionConfiguration t0 sessionState (ts - t0)
-  where
-    updateMeterValuesStateMachine :: SessionState -> SessionState
-    updateMeterValuesStateMachine newState@(Charging {transactionId, currentOffered, energyDelivered, meterValuesStateMachine = (NextMeterValueSampleDue sampleTs)})
-      | ts < sampleTs = newState
-      | otherwise =
-          newState
-            { meterValuesStateMachine =
-                Sampled
-                  ts
-                  MeterValues
-                    { mvTransactionId = transactionId,
-                      mvStationId = stationId,
-                      mvConnectorId = connectorId,
-                      mvTimestamp = ts,
-                      mvCurrents = instantaneousCurrent ts newState,
-                      mvOfferedCurrent = currentOffered,
-                      mvEnergyDelivered = energyDelivered
-                    }
-            }
-    updateMeterValuesStateMachine newState@(Charging {transactionId, currentOffered, energyDelivered, meterValuesStateMachine = (Sampled sampleTs _)})
-      -- TODO: send those meter values somewhere!
-      | ts > (sampleTs + 100) = newState {meterValuesStateMachine = NextMeterValueSampleDue $ max (ts + 1) (sampleTs + meterValuesPeriodicity)}
-      | otherwise =
-          newState
-            { meterValuesStateMachine =
-                Sampled
-                  ts
-                  MeterValues
-                    { mvTransactionId = transactionId,
-                      mvStationId = "which station?",
-                      mvConnectorId = 255,
-                      mvTimestamp = ts,
-                      mvCurrents = instantaneousCurrent ts newState,
-                      mvOfferedCurrent = currentOffered,
-                      mvEnergyDelivered = energyDelivered
-                    }
-            }
-stepSessionState _ _ _ _ = error "Can't simulate time backwards"
-
-stepSimulation :: SimState -> Timestamp -> SimState
+stepSimulation :: SimState -> Timestamp -> (OutputEvent [] MeterValues, SimState)
 stepSimulation (SimState {tick = t0}) ts
-  | ts <= 0 = error "Can't simulate time backwards"
+  | ts <= t0 = error "Can't simulate time backwards"
 stepSimulation
-  simState@(SimState {tick = t0, session = (Session sessionConfiguration sessionState@(Charging {}))})
-  ts = simState {tick = ts, session = Session sessionConfiguration $ stepSessionState t0 sessionConfiguration sessionState ts}
-stepSimulation simState ts = simState {tick = ts}
+  simState@(SimState {tick = t0, session = session@(Session sessionConfiguration sessionState@(Charging {}))})
+  ts = do
+    newState <- stepSessionState session (t0 `durationUntil` ts) $ InputEvent {ieTick = ts, ieTrigger = Nothing}
+    return simState {tick = ts, session = Session sessionConfiguration newState}
+stepSimulation simState ts = return simState {tick = ts}
 
-data InputEvent a = InputEvent
-  { ieTick :: !Timestamp,
-    ieTrigger :: !(Maybe a)
+data SetChargingProfile = SetChargingProfile
+  { -- | transactionId for which the setpoint should be applied
+    scpTransactionId :: !Word64,
+    scpCurrentOffered :: !Double
   }
   deriving (Show, Eq)
 
-data OutputEvent b = OutputEvent
-  { -- | Request for next step no later than after this duration
-    oeNext :: !Duration,
-    oeEvent :: !(Maybe b)
-  }
-  deriving (Show, Eq)
+-- https://github.com/lexi-lambda/mtl-style-example/blob/master/library/MTLStyleExample/Interfaces.hs
 
+simulateChargingPoint :: Session -> InputEvent SetChargingProfile -> (OutputEvent [] MeterValues, ())
+simulateChargingPoint (Session (SessionConfiguration {connectorId, stationId, getInstantaneousCurrent}) sessionState) (InputEvent {ieTick}) = do
+  case sessionState of
+    Charging {transactionId, energyDelivered, currentOffered} -> do
+      tellNextSimulationStepAndEvent (minutes 2 <> seconds 14) $
+        MeterValues
+          { mvTransactionId = transactionId,
+            mvStationId = stationId,
+            mvConnectorId = connectorId,
+            mvTimestamp = ieTick,
+            mvCurrents = getInstantaneousCurrent ieTick sessionState,
+            mvOfferedCurrent = currentOffered,
+            mvEnergyDelivered = energyDelivered
+          }
+      return ()
+    _ -> tellNextSimulationStep $ minutes 1
+
+stepSessionState :: Session -> Duration -> InputEvent SetChargingProfile -> (OutputEvent [] MeterValues, SessionState)
+stepSessionState (Session (SessionConfiguration {charge, getInstantaneousCurrent, meterValuesPeriodicity, stationId, connectorId}) sessionState) stepSize (InputEvent {ieTick = now}) = do
+  sessionState <- charge sessionState stepSize now
+  let sampleDelay = milliseconds 100 -- delay between taking the MeterValues sample and making the output event
+      shortestDelayUntilNextSample = milliseconds 1 -- how long to wait before making new MeterValues sample if we are late
+      updateMeterValuesStateMachine :: SessionState -> (OutputEvent [] MeterValues, SessionState)
+      updateMeterValuesStateMachine newState@(Charging {transactionId, currentOffered, energyDelivered, meterValuesStateMachine = (NextMeterValueSampleDue sampleTs)})
+        | now < sampleTs = return newState
+        | otherwise = do
+            tellNextSimulationStep sampleDelay
+            return $
+              newState
+                { meterValuesStateMachine =
+                    Sampled
+                      now
+                      MeterValues
+                        { mvTransactionId = transactionId,
+                          mvStationId = stationId,
+                          mvConnectorId = connectorId,
+                          mvTimestamp = now,
+                          mvCurrents = getInstantaneousCurrent now newState,
+                          mvOfferedCurrent = currentOffered,
+                          mvEnergyDelivered = energyDelivered
+                        }
+                }
+      updateMeterValuesStateMachine newState@(Charging {transactionId, currentOffered, energyDelivered, meterValuesStateMachine = sampled@(Sampled sampleTs sampledValue)})
+        | now >= (sampleDelay `after` sampleTs) = do
+            let idealNextSample = meterValuesPeriodicity `after` sampleTs
+            let (nextSampleDue, delayToNextSample) =
+                  if now < idealNextSample
+                    then (idealNextSample, now `durationUntil` idealNextSample)
+                    else (shortestDelayUntilNextSample `after` now, shortestDelayUntilNextSample)
+            tellNextSimulationStepAndEvent delayToNextSample sampledValue
+            return $ newState {meterValuesStateMachine = NextMeterValueSampleDue nextSampleDue}
+        | otherwise = do
+            tellNextSimulationStep $ now `durationUntil` (sampleDelay `after` sampleTs)
+            return newState
+      updateMeterValuesStateMachine newState = return newState
+   in updateMeterValuesStateMachine sessionState
--- test/MyLibTest.hs
+++ test/MyLibTest.hs
@@ -2,8 +2,10 @@ module Main (main) where
 
 import ChargingStation
 import Control.Monad (forM_)
+import Control.Monad.Writer (MonadWriter)
+import Control.Monad.Writer.Strict (runWriter, runWriterT)
 import Data.Text (Text)
-import Test.Tasty
+import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit
 
 -- | round x to n digits
@@ -15,6 +17,18 @@ assertApproxEqual :: String -> Int -> Double -> Double -> Assertion
 assertApproxEqual prefix n observed expected =
   assertEqual prefix (approx n expected) (approx n observed)
 
+iterateN :: (Monad m) => (a -> m a) -> Word -> a -> m [a]
+iterateN f = go []
+  where
+    go xs 0 _ = return $ reverse xs
+    go xs n x = f x >>= go (x : xs) (n - 1)
+
+iterateWriter :: (Show w, Monoid w) => (a -> (w, a)) -> Word -> a -> ([w], [a])
+iterateWriter f n a = go ([], [a]) n a
+  where
+    go (ws, xs) 0 _ = (reverse ws, reverse xs)
+    go (ws, xs) n x = let (w, a) = f x in go (w : ws, a : xs) (n - 1) a
+
 main :: IO ()
 main = defaultMain manuallyCheckedComputationTests
 
@@ -24,54 +38,54 @@ manuallyCheckedComputationTests =
     "Manually checked computation"
     [ testCase "Just before reaching 80% charge" $ do
         let SimState {tick = ts, session = Session _ sess} = states !! 1
-        ts @?= 60000
+        ts @?= Timestamp 60000
         assertApproxEqual "batteryLevel" 3 (batteryLevel sess) 64000.0
         assertApproxEqual "energyDelivered" 3 (energyDelivered sess) 10122.666666,
       testCase "Just after reaching 80% charge" $ do
         let SimState {tick = ts, session = Session _ sess} = states !! 2
-        ts @?= 120000
+        ts @?= Timestamp 120000
         assertApproxEqual "batteryLevel" 3 (batteryLevel sess) 64092.0
         assertApproxEqual "energyDelivered" 3 (energyDelivered sess) 10245.333333,
       testCase "No meter values before 5min" $ do
-        forM_ (takeWhile (\(SimState {tick}) -> tick < meterValuesPeriodicity) states) $ \(SimState {tick, session = session@(Session _ (Charging {meterValuesStateMachine = (NextMeterValueSampleDue nextMeterValueTs)}))}) -> do
-          assertEqual "Next meter values sample due" meterValuesPeriodicity nextMeterValueTs,
+        forM_ (takeWhile (\(SimState {tick}) -> tick < (meterValuesPeriodicity `after` startTime)) states) $
+          \(SimState {tick, session = session@(Session _ (Charging {meterValuesStateMachine = (NextMeterValueSampleDue nextMeterValueTs)}))}) -> do
+            assertEqual "Next meter values sample due" (meterValuesPeriodicity `after` startTime) nextMeterValueTs,
       testCase "Meter value sampled after 5min" $ do
-        let (SimState {tick, session = session@(Session _ (Charging {meterValuesStateMachine = (Sampled sampleTimestamp sampledValues)}))} : _) = dropWhile (\(SimState {tick}) -> tick < meterValuesPeriodicity) states
-        assertEqual "Sampled at" meterValuesPeriodicity sampleTimestamp
+        let (SimState {tick, session = session@(Session _ (Charging {meterValuesStateMachine = (Sampled sampleTimestamp sampledValues)}))} : _) =
+              dropWhile (\(SimState {tick}) -> tick < (meterValuesPeriodicity `after` startTime)) states
+        assertEqual "Sampled at" (meterValuesPeriodicity `after` startTime) sampleTimestamp
         assertApproxEqual "Energy delivered" 3 (mvEnergyDelivered sampledValues) 10609.380,
       testCase "Meter value sampled after 10min" $ do
-        let (SimState {tick, session = session@(Session _ (Charging {meterValuesStateMachine = (Sampled sampleTimestamp sampledValues)}))} : _) = dropWhile (\(SimState {tick}) -> tick < 2 * meterValuesPeriodicity) states
-        assertEqual "Sampled at" (2 * meterValuesPeriodicity) sampleTimestamp
+        let (SimState {tick, session = session@(Session _ (Charging {meterValuesStateMachine = (Sampled sampleTimestamp sampledValues)}))} : _) =
+              dropWhile (\(SimState {tick}) -> tick < ((meterValuesPeriodicity <> meterValuesPeriodicity) `after` startTime)) states
+        assertEqual "Sampled at" ((meterValuesPeriodicity <> meterValuesPeriodicity) `after` startTime) sampleTimestamp
         assertApproxEqual "Energy delivered" 3 (mvEnergyDelivered sampledValues) 11203.159
     ]
   where
-    meterValuesPeriodicity = 5 * 60 * 1000
+    startTime = Timestamp 0
+    meterValuesPeriodicity = minutes 5
     sessConf =
       SessionConfiguration
         { batteryCapacity = 80000.0,
-          sessionTarget = LeaveAtTick $ 60 * 20 * 1000, -- 20 min charging time
+          sessionTarget = LeaveAtTick $ minutes 20 `after` startTime,
           charge = chargeEfficientlyUntil80Percent sessConf,
-          instantaneousCurrent = instantaneousCurrentMaxUntil80Percent 16 0.2 sessConf,
+          getInstantaneousCurrent = getInstantaneousCurrentMaxUntil80Percent 16 0.2 sessConf,
           phases = RT,
           stationId = "station_id",
           connectorId = 1,
           meterValuesPeriodicity = meterValuesPeriodicity
         }
-    stepByMinute :: SimState -> SimState
-    stepByMinute simState@(SimState {tick}) = stepSimulation simState $ tick + 1000 * 60
-    states :: [SimState]
+    sessState =
+      Charging
+        { batteryLevel = 64000.0 - 104.266666,
+          energyDelivered = 10000.0,
+          currentOffered = 20,
+          transactionId = 1234321,
+          meterValuesStateMachine = NextMeterValueSampleDue $ meterValuesPeriodicity `after` startTime
+        }
+    stepByMinute :: SimState -> (OutputEvent [] MeterValues, SimState)
+    stepByMinute simState@(SimState {tick}) = stepSimulation simState $ minutes 1 `after` tick
     states =
-      let tick = 0
-       in iterate stepByMinute $
-            SimState
-              { tick = tick,
-                session =
-                  Session sessConf $
-                    Charging
-                      { transactionId = 12341234,
-                        batteryLevel = 64000.0 - 104.266666,
-                        energyDelivered = 10000.0,
-                        currentOffered = 20,
-                        meterValuesStateMachine = NextMeterValueSampleDue $ tick + meterValuesPeriodicity
-                      }
-              }
+      snd $
+        iterateWriter stepByMinute 25 $
+          SimState {tick = startTime, session = Session sessConf sessState}
